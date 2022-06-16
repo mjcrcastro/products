eae1944936bb3c6ab727c0aa1514e236714af0b1
MC <MC@MC-PC.mshome.net>
2022-06-16 15:50:08 -0600

    formmating invoice printing layout

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -98,40 +98,40 @@
  * a datatables jQuery plugin on table id="example"
  */
 $(document).ready(function () {
-    var editButton = $('#editProduct');
-    var deleButton = $('#deleteProduct');
-    var table = $('#productsTable').DataTable({
-        "processing": true,
+var editButton = $('#editProduct');
+var deleButton = $('#deleteProduct');
+var table = $('#productsTable').DataTable({
+"processing": true,
         "serverSide": true,
         "select": {
-            style: 'single'
+        style: 'single'
         },
         "ajax": {
-            "url": "{{ url('/products_ajax') }}",
-            "type": "GET",
-            'headers': {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
+        "url": "{{ url('/products_ajax') }}",
+                "type": "GET",
+                'headers': {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
         },
         "columnDefs": [
-            {
-                "targets": [0],
+        {
+        "targets": [0],
                 "visible": false,
                 "searchable": false
-            }
+        }
         ],
         "columns": [//tells where (from data) the columns are to be placed
-            {"data": "id"},
-            {"data": "barcode"},
-            {"data": "description"},
-            {"data": "price"},
-            {"data": null,
-                "render": function (data, type, row) {
-                    return data['price'];
-                }}
+        {"data": "id"},
+        {"data": "barcode"},
+        {"data": "description"},
+        {"data": "price"},
+        {"data": null,
+                "render": { function (data, type, row) {
+                return data['price'];}
+                }
         ]
-    });
-    table //here we change 
-            .on('select', function (e, dt, type, indexes) {
+        });
+        table //here we change 
+                .on('select', function (e, dt, type, indexes) {
                 var rowData = table.rows(indexes).data().toArray();
                 editButton.html('<a class="btn btn-block text-nowrap btn-primary" href="/products/' + rowData[0]['id'] + '/edit" role="button">Editar <svg class="bi" width="24" height="24" fill="currentColor"><use xlink:href="/vendor/bootstrap/img/bootstrap-icons.svg#pencil-square"/></svg></a>');
                 deleButton.html('<form method="POST" action="/products/' + rowData[0]['id'] + '" accept-charset="UTF-8">' +
@@ -139,15 +139,15 @@ $(document).ready(function () {
                         '<input name="_token" type="hidden" value="' + $('meta[name="csrf-token"]').attr('content') + '">' +
                         '<button class="btn btn-block text-nowrap btn-primary " onclick="if(!confirm(&#039;Are you sure to delete this item?&#039;)){return false;};" type="submit" value="Delete">Borrar <svg class="bi" width="24" height="24" fill="currentColor"><use xlink:href="/vendor/bootstrap/img/bootstrap-icons.svg#x-circle"/></svg></button>' +
                         '</form>');
-            })
-            .on('deselect', function (e, dt, type, indexes) {
+                })
+                .on('deselect', function (e, dt, type, indexes) {
                 editButton.html('<a class="btn btn-block text-nowrap btn-disabled" href="#" role="button">Editar <svg class="bi" width="24" height="24" fill="currentColor"><use xlink:href="/vendor/bootstrap/img/bootstrap-icons.svg#pencil-square"/></svg></a>');
                 deleButton.html('<a class="btn btn-block text-nowrap btn-disabled" href="#" role="button">Borrar <svg class="bi" width="24" height="24" fill="currentColor"><use xlink:href="/vendor/bootstrap/img/bootstrap-icons.svg#x-circle"/></svg></a>');
-            })
-            .on('search.dt', function () {
+                })
+                .on('search.dt', function () {
                 table.rows('.selected').deselect();
-            });
-});
+                });
+        });
 </script>
 
 
