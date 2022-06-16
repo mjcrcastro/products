4e885792e8be757ae74b3ca8a70c3ba53b086f2a
MC <MC@MC-PC.mshome.net>
2022-06-16 16:00:36 -0600

    adding QR to product table

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -128,7 +128,7 @@ var table = $('#productsTable').DataTable({
         {"data": "price"},
         {"data": null,
                 "render": { function (data, type, row) {
-                return ('<img src="https://chart.googleapis.com/chart?chs=400x400&cht=qr&&chld=H&chl=' + data['barcode'] + 'alt="QR code" class="img-fluid">');}
+                return ('<img src="https://chart.googleapis.com/chart?chs=400x400&cht=qr&&chld=H&chl=' + data['barcode'] + '" alt="QR code" class="img-fluid">');}
                 }
         });
         table //here we change 
