a8c8d71153742ec4233320ff1ac803df3defddce
mjcrcastro <mjcrcastro@users.noreply.github.com>
2022-06-18 09:51:23 -0600

    leaving select2 open is better than closing if atfer opening it

--- resources/views/purchases/create.blade.php
+++ resources/views/purchases/create.blade.php
@@ -193,7 +193,7 @@ $(document).ready(function () {
         $('#amountId').val(null);
         $('#costId').val(null);
         
-        $('#productSelect').select2('open').select2('close');
+        $('#productSelect').select2('open');
 
         counter++;
 
